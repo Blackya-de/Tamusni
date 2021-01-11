73d2e005c41b7621cac448e8702b59498a42e683
Blackya-de <yacinegauchi@gmail.com>
2021-01-11 23:56:51 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const textEl = document.getElementById('text')
+const text = 'Tamusni'
+let idx = 1
+let speed = 300
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    setTimeout(writeText, speed)
+}
+
+
+const boxes = document.querySelectorAll('.semestre-r,.semestre-l')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
